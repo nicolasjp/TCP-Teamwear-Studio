98eb99e3d5a2b5c65926e11563c8f9fe6fab1810
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-03-28 15:43:22 +0100

    première version site custom

---
+++ src/Overlay.jsx
@@ -0,0 +1,113 @@
+import { Logo } from '@pmndrs/branding'
+import { motion, AnimatePresence } from 'framer-motion'
+import { AiFillCamera, AiOutlineArrowLeft, AiOutlineHighlight, AiOutlineShopping } from 'react-icons/ai'
+import { useSnapshot } from 'valtio'
+import { state } from './store'
+import React, { useRef, useState } from 'react'
+import { FaTshirt } from 'react-icons/fa';
+
+export function Overlay() {
+  const snap = useSnapshot(state)
+  const transition = { type: 'spring', duration: 0.8 }
+  const config = {
+    initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
+    animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
+    exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } }
+  }
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <motion.header initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y: 0 }} transition={transition}>
+        {/* <Logo width="40" height="40" /> */}
+        <img src="../public/logoTCP.png" alt="logo" width="89.5" height="75.5" /> 
+        <motion.div animate={{ x: snap.intro ? 0 : 100, opacity: snap.intro ? 1 : 0 }} transition={transition}>
+          <FaTshirt size="3em" color='white' />
+        </motion.div>
+      </motion.header>
+      <AnimatePresence>
+        {snap.intro ? (
+          <motion.section key="main" {...config}>
+            <div className="section--container">
+              <motion.div
+                key="title"
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ type: 'spring', damping: 5, stiffness: 40, restDelta: 0.001, duration: 0.3 }}>
+                <h1 style={{ color: 'white' }}>LET'S DO IT.</h1>
+              </motion.div>
+              <div className="support--content">
+                <motion.div
+                  key="p"
+                  initial={{ y: 100, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{
+                    type: 'spring',
+                    damping: 7,
+                    stiffness: 30,
+                    restDelta: 0.001,
+                    duration: 0.6,
+                    delay: 0.2,
+                    delayChildren: 0.2
+                  }}>
+                  <p >
+                    Elevate your tennis style with custom gear. <strong style={{ color: 'white' }}>Unleash your creativity</strong> with our 3D tool. 
+                    Serve up your unique look. Game on, <strong style={{ color: 'white' }}>TCP !</strong>
+                  </p>
+                  <button style={{ background: snap.color }} onClick={() => (state.intro = false)}>
+                    CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
+                  </button>
+                </motion.div>
+              </div>
+            </div>
+          </motion.section>
+        ) : (
+          <motion.section key="custom" {...config}>
+            <Customizer />
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
+
+function Customizer() {
+  const snap = useSnapshot(state)
+  return (
+    <div className="customizer">
+      <div className="color-options">
+        {snap.colors.map((color) => (
+          <div key={color} className={`circle`} style={{ background: color }} onClick={() => (state.color = color)}></div>
+        ))}
+      </div>
+      <div className="decals">
+        <div className="decals--container">
+          {snap.decals.map((decal) => (
+            <div key={decal} className={`decal`} onClick={() => (state.decal = decal)}>
+              <img src={decal + '_thumb.png'} alt="brand" />
+            </div>
+          ))}
+        </div>
+      </div>
+      <button
+        className="share"
+        style={{ background: snap.color }}
+        onClick={() => {
+          const link = document.createElement('a')
+          link.setAttribute('download', 'canvas.png')
+          link.setAttribute('href', document.querySelector('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream'))
+          link.click()
+        }}>
+        DOWNLOAD
+        <AiFillCamera size="1.3em" />
+      </button>
+      <button className="exit" style={{ background: snap.color }} onClick={() => (state.intro = true, state.hoodie = true)}>
+        GO BACK
+        <AiOutlineArrowLeft size="1.3em" />
+      </button>
+
+      <button className="switch" style={{ background: snap.color}} onClick={() => (state.hoodie = !state.hoodie)}>
+        Switch
+        <FaTshirt size="1.3em" />
+      </button>
+    </div>
+  )
+}
---
+++ src/store.jsx
@@ -0,0 +1,12 @@
+import { proxy } from 'valtio'
+
+const state = proxy({
+  intro: true,
+  colors: ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934', '#4C4CFF', '#1FA055', '#FD6C9E', '#01796F', '#B22222', '#94718E', '#6495ED'],
+  decals: ['logoV1', 'logoV2', 'logoV3', 'logoV4'],
+  color: '#EF674E',
+  decal: 'logoV1',
+  hoodie: true,
+})
+
+export { state }
